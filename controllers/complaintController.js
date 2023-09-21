c0671bb8251a489b4a1f1f68cff7d1e4560c9e6e
rahul14kumar <rahul.kumar.it25@heritageit.edu.in>
2023-09-21 19:22:09 +0530

    compalin controller

---
+++ controllers/complaintController.js
@@ -0,0 +1,141 @@
+const expressAsyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const userModel = require("../models/userSchema");
+const complaint=require("../models/complaintSchema");
+
+// create a complaint done bty citizen
+const createComplaint=expressAsyncHandler(async(req,res)=>{
+    // const {adharCardNumber}=req.user
+    const {raisedBy,description,image,deptToSend,district}=req.body;
+    try{
+        const complain=await complaint.create({
+            raisedBy,description,image,deptToSend,district
+        });
+
+        res.status(200).json(complain);
+    }catch(error){
+        res.status(500).json(error);
+    }
+})
+
+// the complaint which is seen by nodal officer
+const complaintSeen=expressAsyncHandler(async(req,res)=>{
+    try{
+        const {designation,name,email,role}=req.user;
+        const complain=await complaint.find({district:designation[1],pathToTravel:[]});
+
+        if(complain){
+            res.status(200).json(complain);
+        }else{
+            res.status(404).json("no compliant found");
+        }
+
+    }catch(error){
+        res.status(500).json(error);
+    }
+})
+
+// add pathToTravel and description of particular complaint done byNodalOfficer
+const createPathToTravel=expressAsyncHandler(async(req,res)=>{
+    const complainId=req.params.id;
+    const {designation,name,email,role}=req.user;
+    const {department,description,post}=req.body;
+
+    const complain=await complaint.findById(complainId);
+
+    try{
+        if(designation[0]=="nodalOfficer" && complain){
+            complain.pathToTravel.push({
+                assignedDept:department,
+                assignedDist:complain.district,
+                assignedPost:post
+            })
+
+            complain.descriptionByNodalOfficer=description;
+            await complain.save();
+
+            res.status(200).json("path and desciption has been added to complain");
+        }else{
+            res.status(404).json("you are not required to perform opertion");
+        }
+    }catch(error){
+        res.status(500).json(error);
+    }
+})
+
+// add commments to a particular complaint
+const addComment=expressAsyncHandler(async(req,res)=>{
+    const complainId=req.params.id;
+    const {email,name,role}=req.user
+    const {comment}=req.body
+    try{
+        const complain=await complaint.findById(complainId);
+        if(complain){
+            complain.comments.push({
+                comment:comment,
+                commentBy:name
+            })
+
+            await complain.save();
+            res.status(200).json("add commment on complain");
+        }
+    }catch(error){
+        res.status(500).json(error);
+    }
+})
+
+//show thw all complain belong to the particular officer/department in the partcular district
+const departmentComplainBoard=expressAsyncHandler(async(req,res)=>{
+    const {designation,name,email}=req.user;
+    try{
+       const complain=await complaint.find({district:designation[1]});
+       if(complain){
+        const filterComplaint=complain.filter(comp=>{
+            return comp.pathToTravel.some(dept=>designation[2]===dept.assignedDept);
+        })
+        if(filterComplaint){
+            return res.status(200).json(filterComplaint);
+        }else{
+            return res.json(404).json("no filter complain found");
+        }
+       }else{
+        console.log("no complain found");
+        return res.status(400).json("no complain found")
+       }
+    }catch(error){
+        res.status(500).json(error);
+    }
+})
+
+// particular officer update the the status of complaint solved
+const updateStatus=expressAsyncHandler(async(req,res)=>{
+    const complainId=req.params.id;
+    // see which officer logged in
+    const {designation,name,email}=req.user;
+    try{
+        const complain=await complaint.findById(complainId);
+        if(complain){
+            const isComplain=complain.pathToTravel.find(compl=>compl.assignedDept===designation[2]);
+
+            if(isComplain){
+                complain.pathToTravel
+            }
+        }
+    }catch(error){
+        res.status(500).json(error);
+    }
+})
+
+// complain to travel just like the path given or complain seen by hte officers who are in the path to travel.
+
+const toTravel=expressAsyncHandler(async(req,res)=>{
+
+})
+
+module.exports={
+    createComplaint,
+    complaintSeen,
+    createPathToTravel,
+    addComment,departmentComplainBoard
+}
---
+++ models/complaintSchema.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const nodalSchema=new Schema({
+    assignedDept:{
+        type:String
+    },
+    assignedDist:{
+        type:String
+    },
+    assignedPost:{
+        type:String
+    },
+    markDone:{
+        type:Boolean,
+        default:false
+    }
+});
+
+const commentSchema=new Schema({
+    comment:{
+        type:String
+    },
+    commentBy:{
+        type:String
+    }
+})
+
+const complaintSchema=new Schema({
+    raisedBy: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+    deptToSend:{
+        type:String,
+    },
+    district:{
+        type:String,
+    },
+    pathToTravel:{
+        type:[nodalSchema]
+    },
+    descriptionByNodalOfficer:{
+        type:String
+    },
+    comments:{
+        type:[commentSchema]
+    },
+    status:{
+        type:Boolean,
+        default:false
+    },
+    partiallySolved:{
+        type:Boolean,
+        default:false
+    }
+},  { 
+    timeStamps: true 
+})
+module.exports = mongoose.model("complaints", complaintSchema);
