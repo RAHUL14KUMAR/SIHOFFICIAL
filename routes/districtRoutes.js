dd28b22aeea837690cf4c93fd04079aadc36f5e9
rahul14kumar <rahul.kumar.it25@heritageit.edu.in>
2023-09-22 21:31:55 +0530

    getAllDistrict

--- routes/districtRoutes.js
+++ routes/districtRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 // const { createDepartment } = require("../controllers/departmentController");
 const protect = require("../middleware/authMiddleware");
-const { createDistrict } = require("../controllers/districtController");
+const { createDistrict, getAllDistrict } = require("../controllers/districtController");
 
 const router = express.Router();
 
 router.post("/addDistrict", protect, createDistrict);
+
+router.get("/getDistrict",protect,getAllDistrict);
 
 module.exports = router;
